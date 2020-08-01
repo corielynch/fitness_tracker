1ee75e7cbb2dd3014e1bc4e7c5f621ee9328e85b
Corie Lynch <Lynchc17@gmail.com>
2020-07-31 22:01:47 -0700

    html routes and api routes

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes"));
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
